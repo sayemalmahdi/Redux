96ba5f8f6944a3d23cd22c67baf83a6378d131c1
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-02-24 23:59:10 +0600

    React State Management issues

--- react-counter-application/src/components/Counter.js
+++ react-counter-application/src/components/Counter.js
@@ -1,36 +1,14 @@
-import React, { useState } from 'react'
+import Button from "./Button";
+import Count from "./Count";
 
-const Counter = () => {
-    const[count, setCount] = useState(0);
-
-    const increment = () => {
-        setCount((prevCount) => prevCount + 1);
-    }
-    const decrement = () => {
-        setCount((prevCount) => prevCount - 1);
-    }
-
-  return (
-    <div
-        class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-    >
-        <div class="text-2xl font-semibold">{count}</div>
-        <div class="flex space-x-3">
-            <button
-                class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                onClick={increment}
-            >
-                Increment
-            </button>
-            <button
-                class="bg-red-400 text-white px-3 py-2 rounded shadow"
-                onClick={decrement}
-            >
-                Decrement
-            </button>
+export default function Counter({ id, count, increment, decrement }) {
+    return (
+        <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+            <Count count={count} />
+            <div class="flex space-x-3">
+                <Button handler={() => increment(id)}>Increment</Button>
+                <Button handler={() => decrement(id)}>Decrement</Button>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
-
-export default Counter
