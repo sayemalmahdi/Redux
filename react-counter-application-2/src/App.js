2fa07cd0073dcf9847bfccb2cbfce8091b563964
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-03-01 23:43:07 +0600

    Added 'Simple Counter Application 2'

---
+++ react-counter-application-2/src/App.js
@@ -0,0 +1,18 @@
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">         
+            <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+                Simple Counter Application 2
+            </h1>
+      
+            <div class="max-w-md mx-auto mt-10 space-y-5">
+                <Counter />
+                <Counter />
+            </div>
+        </div>
+  );
+}
+
+export default App;
