bf304555c27075adef041d00b059f82aa2de43fb
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-03-02 20:21:17 +0600

    Creating Store & Providing Store To React Application

---
+++ react-counter-application-2/src/redux/store.js
@@ -0,0 +1,7 @@
+import { legacy_createStore as createStore} from 'redux'
+
+import counterReducer from "./counter/counterReducer";
+
+const store = createStore(counterReducer);
+
+export default store;
