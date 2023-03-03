5c8c8e761f041f8b0b631513f932340a4bc1afc5
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-03-03 22:18:29 +0600

    Connect API | mapStateToProps() and mapDispatchToProps()

--- react-counter-application-2/src/App.js
+++ react-counter-application-2/src/App.js
@@ -5,14 +5,13 @@ import store from "./redux/store";
 function App() {
   return (
     <Provider store={store}>
-      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">         
-            <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">         
+            <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
                 Simple Counter Application 2
             </h1>
       
-            <div class="max-w-md mx-auto mt-10 space-y-5">
-                <Counter />
-                <Counter />
+            <div className="max-w-md mx-auto mt-10 space-y-5">
+              <Counter id="1" />
             </div>
       </div>
     </Provider>
